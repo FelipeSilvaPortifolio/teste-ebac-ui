d59a1056ab7a3eedfbec0b07e260e27f8dd2770f
FelipeSilvaPortifolio <portifoliofelipesilva@gmail.com>
2025-01-26 22:23:02 -0300

    testes funcionalidade produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/');
+    });
+
+    it('Deve selecionar o primeiro produto da lista', () => {
+        cy.get('.product-block').first().click();
+        cy.get('.product_title').should('exist');
+    });
+
+    it('Deve selecionar o último produto da lista', () => {
+        cy.get('.product-block').last().click();    
+        cy.get('.product_title').should('exist');
+    });
+
+    it('Deve selecionar um produto indicado na lista', () => {
+        cy.get('.product-block').eq(2).click();
+        cy.get('.product_title').should('exist');
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block').contains('Ajax Full-Zip Sweatshirt').click();
+        cy.get('.product_title').should('exist');
+    });
+});
