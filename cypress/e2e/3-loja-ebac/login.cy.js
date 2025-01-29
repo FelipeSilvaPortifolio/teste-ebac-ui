adfb2e107d00953933983695d9aae5298fe385cc
FelipeSilvaPortifolio <portifoliofelipesilva@gmail.com>
2025-01-28 21:05:54 -0300

    Atualização do códico com Arquivo de Dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,20 +1,21 @@
 /// <reference types="cypress" />
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
-    afterEach(() => {
-        cy.screenshot()
-    });
+    ///afterEach(() => {
+       /// cy.screenshot()
+    ///});
 
     it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('felipesilva@teste.com')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist', 'Olá, felipesilva')   
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain.text', 'Olá, felipesilva')  
     });
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
@@ -30,4 +31,22 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('exist')
     });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain.text', 'Olá, felipesilva')  
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then( dados => {
+            cy.get('#username').type(dados.usuario, { log: false })
+            cy.get('#password').type(dados.senha, { log: false })
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain.text', 'Olá, felipesilva')  
+        })
+    });
+
+
 })
