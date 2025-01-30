6e8b9c3ec618e96c7ead674180c3e5e286b98105
FelipeSilvaPortifolio <portifoliofelipesilva@gmail.com>
2025-01-29 21:00:21 -0300

    Atualização de exemplos com Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,13 +1,15 @@
 /// <reference types="cypress" />
+import produtosPage from '../../support/page-objects/produtos.page';
+
 
 describe('Funcionalidade: Produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos');
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar o primeiro produto da lista', () => {
-        cy.get('.product-block').first().click();
+        produtosPage.buscaProdutoLista('Ariel Roll Sleeve Sweatshirt');
         cy.get('.product_title').should('exist');
     });
 
@@ -24,5 +26,33 @@ describe('Funcionalidade: Produtos', () => {
     it('Deve selecionar um produto da lista', () => {
         cy.get('.product-block').contains('Ajax Full-Zip Sweatshirt').click();
         cy.get('.product_title').should('exist');
+    });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Abominable Hoodie';
+        produtosPage.buscarProduto(produto);
+        cy.get('.product_title').should('contain', produto);
+    });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Apollo Running Short')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Red', 7)
+
+        cy.get('.woocommerce-message').should('exist')
+    });
+
+    it.only('Deve adicionar produto ao carrinho - Buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            let produto = 2
+            produtosPage.buscarProduto(dados[produto].nomeProduto)
+            produtosPage.addProdutoCarrinho(dados[produto].tamanho, dados[produto].cor, dados[produto].quantidade)
+    
+            cy.get('.woocommerce-message').should('contain', dados[produto].nomeProduto)
+        })
+
     });
 });
